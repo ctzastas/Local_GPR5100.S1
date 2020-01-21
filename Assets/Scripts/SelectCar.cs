52183e7d35d56f63fbbcb6a356b811867316afcf
Christos Tzastas <ctzastas@gmail.com>
2020-01-21 21:16:03 +0200

    Import select car in main menu

---
+++ Assets/Scripts/SelectCar.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class SelectCar : MonoBehaviour {
+
+    public GameObject[] cars;
+    private int currentCar = 0;
+    
+    // Start is called before the first frame update
+    void Start() {
+        if (PlayerPrefs.HasKey("PlayerCar")) {
+            currentCar = PlayerPrefs.GetInt("PlayerCar");
+        }
+        transform.LookAt(cars[currentCar].transform.position);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            currentCar++;
+            if (currentCar > cars.Length - 1) {
+                currentCar = 0;
+            }
+            PlayerPrefs.SetInt("PlayerCar", currentCar);
+        }
+        Quaternion lookDirection = Quaternion.LookRotation(cars[currentCar].transform.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookDirection, Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/SpinCars.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class SpinCars : MonoBehaviour {
+    
+    [SerializeField] private float spinSpeed = 0.3f;
+    
+    // Update is called once per frame
+    void Update() {
+        transform.Rotate(0,spinSpeed,0);
+    }
+}
